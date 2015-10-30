5028e84e915517c20701a3a9648ffebfef259c61
MukoQ <tamtamr72@gmail.com>
2015-10-31 00:14:07 +0900

    comit!!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,51 @@
 (function() {
-  console.log('\'Allo2 \'Allo2!'); // eslint-disable-line no-console
+  console.log('camera?');
+
+  var video = document.querySelector('video');
+  var canvas = document.querySelector('canvas');
+  var img = document.querySelector('img');
+  var ctx = canvas.getContext('2d');
+  var localMediaStream = null;
+
+  function hasGetUsermedia() {
+    console.log('hasGetUsermedia');
+    return !!(navigator.getUserMedia ||
+              navigator.webkitGetUserMedia ||
+              navigator.mozGetUserMedia ||
+              navigator.msGetUserMedia);
+  }
+
+  if (hasGetUsermedia) {
+    console.log('getUserMedia() is supported in your browser');
+  } else {
+    alert('getUserMedia() is not supported in your browser');
+  }
+
+  navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
+
+  var onFailSoHard = function(e) {
+    console.log('onFailSoHard');
+    console.log('Reeeejected!', e);
+  };
+
+  var videoStart =  function(localMediaStream) {
+    console.log('videoStart');
+    video.src = window.URL.createObjectURL(localMediaStream);
+  };
+
+  navigator.getUserMedia({video: true, audio: false}, videoStart, onFailSoHard);
+
+  function snapshot(localMediaStream) {
+    console.log('snapshot');
+    if (localMediaStream) {
+      ctx.drawImage(video, 0, 0);
+      img.src = canvas.toDataURL('image/webp');
+    }
+  }
+
+  video.addEventListener('click', snapshot, false);
+
 })();
